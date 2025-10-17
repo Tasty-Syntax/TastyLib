e8801ae8a61fc40a1b66b8f848882b3ef0cd9ac2
Viluron <markus.fiederling@gmail.com>
2025-10-17 17:48:52 +0200

    ADDED: Button helpers

---
+++ src/ui/_ui_widget.ts
@@ -0,0 +1,177 @@
+/**
+ * Represents a UI widget.
+ * @author TastySyntax
+ */
+export class _UiWidget {
+  private _name: string;
+
+  /**
+   * Constructs a new UiWidget instance.
+   * @param name - The name of the widget.
+   * @param position - The position of the widget.
+   * @param size - The size of the widget.
+   * @param anchor - The anchor point of the widget.
+   */
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  /**
+   * Gets the name of the widget.
+   * @returns The name of the widget.
+   */
+  get name(): string {
+    return this._name;
+  }
+
+  /**
+   * Gets the position of the widget.
+   * @returns The position of the widget.
+   */
+  get position(): mod.Vector {
+    return mod.GetUIWidgetPosition(this.widget);
+  }
+
+  /**
+   * Gets the size of the widget.
+   * @returns The size of the widget.
+   */
+  get size(): mod.Vector {
+    return mod.GetUIWidgetPosition(this.widget);
+  }
+
+  /**
+   * Gets the anchor point of the widget.
+   * @returns The anchor point of the widget.
+   */
+  get anchor(): mod.UIAnchor {
+    return mod.GetUIWidgetAnchor(this.widget);
+  }
+
+  /**
+   * Gets the widget object.
+   * @returns The widget object.
+   */
+  get widget(): mod.Any {
+    return mod.FindUIWidgetWithName(this.name);
+  }
+
+  /**
+   * Sets the anchor point of the widget.
+   * @param anchor - The new anchor point of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetAnchor(anchor: mod.UIAnchor): _UiWidget {
+    mod.SetUIWidgetAnchor(this.widget, anchor);
+    return this;
+  }
+
+  /**
+   * Sets the alpha value of the widget.
+   * @param alpha - The new alpha value of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetBgAlpha(alpha: number): _UiWidget {
+    mod.SetUIWidgetBgAlpha(this.widget, alpha);
+    return this;
+  }
+
+  /**
+   * Sets the background color of the widget.
+   * @param color - The new background color of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetBgColor(color: mod.Vector): _UiWidget {
+    mod.SetUIWidgetBgColor(this.widget, color);
+    return this;
+  }
+
+  /**
+   * Sets the background fill of the widget.
+   * @param fill - The new background fill of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetBgFill(fill: mod.UIBgFill): _UiWidget {
+    mod.SetUIWidgetBgFill(this.widget, fill);
+    return this;
+  }
+
+  /**
+   * Sets the depth of the widget.
+   * @param depth - The new depth of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetDepth(depth: mod.UIDepth): _UiWidget {
+    mod.SetUIWidgetDepth(this.widget, depth);
+    return this;
+  }
+
+  /**
+   * Sets the name of the widget.
+   * @param name - The new name of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetName(name: string): _UiWidget {
+    mod.SetUIWidgetName(this.widget, name);
+    this._name = name;
+    return this;
+  }
+
+  /**
+   * Sets the padding of the widget.
+   * @param padding - The new padding of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetPadding(padding: number): _UiWidget {
+    mod.SetUIWidgetPadding(this.widget, padding);
+    return this;
+  }
+
+  /**
+   * Sets the parent of the widget.
+   * @param parent - The new parent of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetParent(parent: mod.UIWidget): _UiWidget {
+    mod.SetUIWidgetParent(this.widget, parent);
+    return this;
+  }
+
+  /**
+   * Sets the position of the widget.
+   * @param position - The new position of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetPosition(position: mod.Vector): _UiWidget {
+    mod.SetUIWidgetPosition(this.widget, position);
+    return this;
+  }
+
+  /**
+   * Sets the size of the widget.
+   * @param size - The new size of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetSize(size: mod.Vector): _UiWidget {
+    mod.SetUIWidgetSize(this.widget, size);
+    return this;
+  }
+
+  /**
+   * Sets the visibility of the widget.
+   * @param visible - The new visibility of the widget.
+   * @returns The current instance of UiWidget.
+   */
+  setWidgetVisible(visible: boolean): _UiWidget {
+    mod.SetUIWidgetVisible(this.widget, visible);
+    return this;
+  }
+
+
+  /**
+   * Deletes the widget.
+   */
+  delete(): void {
+    mod.DeleteUIWidget(this.widget);
+  }
+}
---
+++ src/ui/button.ts
@@ -0,0 +1,135 @@
+import { _UiWidget } from "./_ui_widget";
+
+/**
+ * Represents a UI button widget.
+ * @extends _UiWidget
+ * @author TastySyntax
+ */
+export class UiButton extends _UiWidget {
+  /**
+   * Constructs a new UiButton instance.
+   * @param name - The name of the button.
+   * @param position - The position of the button.
+   * @param size - The size of the button.
+   * @param anchor - The anchor point of the button.
+   */
+  constructor(name: string, position: mod.Vector, size: mod.Vector, anchor: mod.UIAnchor, recipient?: mod.Player | mod.Team) {
+    super(name);
+
+    if (recipient) {
+      mod.AddUIButton(name, position, size, anchor, recipient);
+    } else {
+      mod.AddUIButton(name, position, size, anchor);
+    }
+  }
+
+  /**
+   * Sets the alpha value of the button when not hovered or focused.
+   * @param value - The alpha value of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setAlphaBase(value: number) {
+    mod.SetUIButtonAlphaBase(this.widget, value);
+    return this;
+  }
+
+  /**
+   * Sets the alpha value of the button when disabled.
+   * @param value - The alpha value of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setAlphaDisabled(value: number) {
+    mod.SetUIButtonAlphaDisabled(this.widget, value);
+    return this;
+  }
+
+  /**
+   * Sets the alpha value of the button when focused.
+   * @param value - The alpha value of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setAlphaFocused(value: number) {
+    mod.SetUIButtonAlphaFocused(this.widget, value);
+    return this;
+  }
+
+  /**
+   * Sets the alpha value of the button when hovered.
+   * @param value - The alpha value of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setAlphaHover(value: number) {
+    mod.SetUIButtonAlphaHover(this.widget, value);
+    return this;
+  }
+
+  /**
+   * Sets the alpha value of the button when pressed.
+   * @param value - The alpha value of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setAlphaPressed(value: number) {
+    mod.SetUIButtonAlphaPressed(this.widget, value);
+    return this;
+  }
+
+  /**
+   * Sets the color of the button when not hovered or focused.
+   * @param color - The color of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setColorBase(color: mod.Vector) {
+    mod.SetUIButtonColorBase(this.widget, color);
+    return this;
+  }
+
+  /**
+   * Sets the color of the button when disabled.
+   * @param color - The color of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setColorDisabled(color: mod.Vector) {
+    mod.SetUIButtonColorDisabled(this.widget, color);
+    return this;
+  }
+
+  /**
+   * Sets the color of the button when focused.
+   * @param color - The color of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setColorFocused(color: mod.Vector) {
+    mod.SetUIButtonColorFocused(this.widget, color);
+    return this;
+  }
+
+  /**
+   * Sets the color of the button when hovered.
+   * @param color - The color of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setColorHover(color: mod.Vector) {
+    mod.SetUIButtonColorHover(this.widget, color)
+    return this;
+  }
+
+  /**
+   * Sets the color of the button when pressed.
+   * @param color - The color of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setColorPressed(color: mod.Vector) {
+    mod.SetUIButtonColorPressed(this.widget, color)
+    return this;
+  }
+
+  /**
+   * Sets the enabled state of the button.
+   * @param enabled - The enabled state of the button.
+   * @returns - The current instance of UiButton.
+   */
+  setEnabled(enabled: boolean) {
+    mod.SetUIButtonEnabled(this.widget, enabled);
+    return this;
+  }
+}
